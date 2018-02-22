b4c2450d46bb0d18cca48717f944342eab3adc53
Zoe Jadi <ztjadi@gmail.com>
2018-02-22 12:17:54 -0300

    First Push

--- api.py
+++ api.py
@@ -46,6 +46,34 @@ def logout():
    return redirect(url_for('index'))
 
 
+@app.route('/manager', methods=["GET", "POST"])
+def manager_home():
+    if 'id' in session:
+        if session['level'] == 1:
+            try:
+                return render_template('manager_home.html')
+            except Exception as e:
+                return e
+        else:
+            return 'geen acces'
+    else:
+        return 'nope'
+
+
+@app.route('/kassa', methods=["GET", "POST"])
+def kassa_home():
+    if 'id' in session:
+        if session['level'] == 2:
+            try:
+                return render_template('kassa_home.html')
+            except Exception as e:
+                return e
+        else:
+            return 'geen acces'
+    else:
+        return 'nope'
+
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
     app.run(debug=True)
